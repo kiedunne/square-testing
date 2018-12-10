f52e1de7fe1cb814954d3801f0a824f6f877b6fa
kiedunne <kie.dunne@gmail.com>
2018-12-10 12:33:03 +0000

    Added square.js and passing spec

---
+++ spec/SquareSpec.js
@@ -0,0 +1,13 @@
+describe("Square", function() {
+
+  beforeEach(function() {
+    square = new Square(5)
+  });
+
+  it("Calculates area of the square", function() {
+    square.calculateArea()
+    expect(square.area).toEqual(25);
+
+  });
+
+});
